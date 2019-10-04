a42138cda4f7df9d27febafbb636d75c10e7fddb
Jean Ha <jeanha124@gmail.com>
2019-10-04 08:46:40 -0400

    "add minChange"

--- memoization_project/lib/problems.js
+++ memoization_project/lib/problems.js
@@ -39,7 +39,18 @@ function lucasNumberMemo(n, memo = {}) {
 // minChange([1, 5, 10, 25], 15)    // => 2, because 10 + 5 = 15
 // minChange([1, 5, 10, 25], 100)   // => 4, because 25 + 25 + 25 + 25 = 100
 function minChange(coins, amount, memo = {}) {
+    if (amount in memo) return memo[amount];
+    if (amount === 0) return 0;
 
+    let arr = [];
+    coins.forEach(function(coin) {
+        if (coin <= amount) {
+            arr.push(minChange(coins, amount - coin, memo) + 1);
+        }
+    });
+
+    memo[amount] = Math.min(...arr);
+    return memo[amount];
 }
 
 
