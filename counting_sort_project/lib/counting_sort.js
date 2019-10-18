2a10334ed94ca1ac6ae53fb1ee22c607657becab
Jean Ha <jeanha124@gmail.com>
2019-10-18 18:17:54 -0400

    "add counting sort"

--- counting_sort_project/lib/counting_sort.js
+++ counting_sort_project/lib/counting_sort.js
@@ -1,5 +1,18 @@
 function countingSort(arr, max) {
-
+    if (arr.length < 2) return arr;
+    let result = [];
+    let count = new Array(max + 1).fill(0);
+    
+    for (let i = 0; i < arr.length; i++){
+        count[arr[i]] += 1;
+    }
+    for (let i = 0; i < count.length; i++){
+        while (count[i] > 0) {
+            result.push(i);
+            count[i] -= 1;
+        }
+    }
+    return result;
 }
 
 
--- radix_sort_project/lib/radix_sort.js
+++ radix_sort_project/lib/radix_sort.js
@@ -1,5 +1,8 @@
 function radixSort(arr) {
-
+    if (!Array.isArray(arr)) return null;
+    if (arr.length < 2) return arr;
+    // let result = arr.map((el) => el).sort((a, b) => a - b);
+    // return result;
 }
 
 module.exports = {
