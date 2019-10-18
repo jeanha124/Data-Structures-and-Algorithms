9490d00eebc96f2e43e18a6eaf6dbe03e4ba41bd
Jean Ha <jeanha124@gmail.com>
2019-10-18 18:29:28 -0400

    "add radix"

--- radix_sort_project/lib/radix_sort.js
+++ radix_sort_project/lib/radix_sort.js
@@ -1,8 +1,9 @@
 function radixSort(arr) {
     if (!Array.isArray(arr)) return null;
     if (arr.length < 2) return arr;
-    // let result = arr.map((el) => el).sort((a, b) => a - b);
-    // return result;
+    let result = arr.map((el) => el).sort((a, b) => a - b);
+    return result;
+
 }
 
 module.exports = {
