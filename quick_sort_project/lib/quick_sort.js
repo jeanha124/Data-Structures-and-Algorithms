8bde137a57edc49ba727fc08c29fe51b0cb276b8
Jean Ha <jeanha124@gmail.com>
2019-10-18 17:26:24 -0400

    "add quicksort"

--- quick_sort_project/lib/quick_sort.js
+++ quick_sort_project/lib/quick_sort.js
@@ -1,5 +1,11 @@
 function quickSort(array) {
-
+    if (array.length < 2) return array;
+    let pivot = array.shift();
+    let left = array.filter((el) => el < pivot);
+    let right = array.filter((el) => el >= pivot);
+    let leftSorted = quickSort(left);
+    let rightSorted = quickSort(right);
+    return [...leftSorted, pivot, ...rightSorted]
 }
 
 
