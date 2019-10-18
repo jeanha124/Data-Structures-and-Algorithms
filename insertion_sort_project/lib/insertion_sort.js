c341c86bd5e8f7acd6d6574f6d0f474d3ff26c75
Jean Ha <jeanha124@gmail.com>
2019-10-18 15:28:32 -0400

    "push insertion sort"

--- insertion_sort_project/lib/insertion_sort.js
+++ insertion_sort_project/lib/insertion_sort.js
@@ -1,5 +1,14 @@
 function insertionSort(arr) {
-
+    for (let i = 0; i < arr.length; i++) {
+        let temp = arr[i],
+            j = i;
+        while (j > 0 && temp < arr[j - 1]) {
+            arr[j] = arr[j - 1];
+            j -= 1;
+        }
+        arr[j] = temp;
+    }
+    return arr;
 }
 
 module.exports = {
